bcd9cde7c683ed1fbbd569cbaefd90e08f32cee3
Zeezermane <adusman@asu.edu>
2021-11-10 10:25:22 -0700

    React Hook conversion

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,47 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-class Login extends React.Component {
-  state = {
-    userid: "",
-    pwd: "",
-  };
-  handleChange = (e) => {
+export default function Login() {
+  const [userId, setUserId] = useState("")
+  const [pwd, setUserPwd] = useState("")
+  function handleChange(e) {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-  handleSubmit = (e) => {};
-
-  render() {
-    return (
-      <div className="login">
-        <div className="logo">
-          <img src={"/blueflower.png"} alt="picture" />
-        </div>
-        <div>
-          <h1 className="greet"> Welcome </h1>
-          <form onSubmit={this.handleSubmit}>
-            <label for="userid">User ID </label>
-            <input type="text" name="userid" onChange={this.handleChange} />
-            <label for="password"> Password</label>
-            <input type="password" name="pwd" onChange={this.handleChange} />
-            <p className="forgotPwd">
-              {" "}
-              <a href="https://www.google.com/"> Forgot your password? </a>
-            </p>
-            <div class="center">
-              <button id="login-button" onSubmit={this.handleSubmit}>
-                {" "}
-                LOG IN
-              </button>
-              <button id="signUp-button" onSubmit={this.handleSubmit}>
-                SIGN UP
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
   }
-}
+  function handleSubmit(e) {}
 
-export default Login;
+  return (
+    <div className="login">
+      <div className="logo">
+        <img src={"/blueflower.png"} alt="picture" />
+      </div>
+      <div>
+        <h1 className="greet"> Welcome </h1>
+        <form >
+          <label for="userid">User ID </label>
+          <input type="text" name="userid" onChange={(e) => setUserId(e.value)}  value={userId} />
+          <label for="password"> Password</label>
+          <input type="password" name="pwd" onChange={(e) => setUserPwd(e.value)} value={pwd} />
+          <p className="forgotPwd">
+            {" "}
+            <a href="https://www.google.com/"> Forgot your password? </a>
+          </p>
+          <div class="center">
+            <button id="login-button" >
+              {" "}
+              LOG IN
+            </button>
+            <button id="signUp-button" >
+              SIGN UP
+            </button>
+          </div>
+        </form>
+      </div>
+    
+    </div>
+  );
+}
